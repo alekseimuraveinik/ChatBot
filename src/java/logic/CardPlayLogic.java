be59683287aaf03bb2360f8a7ac5b49acb10dbf9
max31000 <49785937+max31000@users.noreply.github.com>
2019-12-03 08:09:21 +0500

    [fix] небольшие доработки, внедрение команд при игре в карты, возможность сдаться

--- src/java/logic/CardPlayLogic.java
+++ src/java/logic/CardPlayLogic.java
@@ -12,8 +12,15 @@ public class CardPlayLogic implements IChatLogic {
     private HashMap<IPlayer, CardPlayState> playersStates = new HashMap<>();
     private Random r = new Random();
 
+    private static final String SLASH = "/";
+    private static final String SPACE = " ";
+    private static final String UNKNOWN_COMMAND = "Эта команда либо сейчас не доступна, либо ее нет";
+    private static final String HELP = "/help";
+    private static final String INVENTORY = "/my_inventory";
+    private static final String LOSE = "/lose";
+
     private final String SPLITTER = ", ";
-    private final String HELLO_MESSAGE = "Добро пожаловать в карточную игру. \nПравила: игроки в закрытую выкладывают по карте. Если сумма четная, карты отдается игроку, у которого число на карте больше, если нечетное - наоборот, если равны - карты возвращаются. Побеждает тот, кто отберет у противника все карты";
+    private final String HELLO_MESSAGE = "Добро пожаловать в карточную игру. \nПравила: игроки в закрытую выкладывают по карте. Если сумма четная, карты отдается игроку, у которого число на карте больше, если нечетное - наоборот, если равны - карты возвращаются. Побеждает тот, кто отберет у противника все карты\nСдаться - /lose";
     private final String POST_HELLO_MESSAGE = "\nВведите номинал карты, которую хотите разыграть";
     private final String WRONG_MESSAGE_ANSWER = "Введите только номер карты";
     private final String PLAYER_WON_MOVE = "Вы победили и забираете себе карты";
@@ -25,6 +32,11 @@ public class CardPlayLogic implements IChatLogic {
     public void processMessage(String message, IPlayer player, GraphNode currentNode) {
         CardPlayState thisPlayerState = playersStates.get(player);
         String processMessage = "";
+
+        if(message.startsWith(SLASH)){
+            player.handle(processCommand(message, player));
+            return;
+        }
 
         if (!IsParseString(message) || !thisPlayerState.cards.contains(Integer.parseInt(message))) {
             player.handle(WRONG_MESSAGE_ANSWER);
@@ -123,5 +135,25 @@ public class CardPlayLogic implements IChatLogic {
         }
 
         return false;
+    }
+
+    private String processCommand(String command, IPlayer player){
+        String answer;
+        String[] commandComponents = command.split(SPACE);
+        switch (commandComponents[0]){
+            case HELP:
+                answer = HELLO_MESSAGE;
+                break;
+            case INVENTORY:
+                answer = player.getPlayerState().getPlayerInventory().stringRepresentation();
+                break;
+            case LOSE:
+                answer = "Вы сдались";
+                switchToDefaultLogic("lose", player);
+                break;
+            default:
+                answer = UNKNOWN_COMMAND;
+        }
+        return answer;
     }
 }
